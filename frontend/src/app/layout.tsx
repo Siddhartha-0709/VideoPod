01c13a48f5f25543d6bfb99818806c7b221f763d
Siddhartha Mukherjee <siddharthamukherjee0709@gmail.com>
2024-12-18 10:56:25 +0530

    social cards

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,6 +9,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>VideoPod</title>
+
+        <meta property="og:title" content="Your Journey to Stunning Videos Starts Now" />
+        <meta property="og:description" content="Experience the ultimate toolkit for effortless video transformation" />
+        <meta property="og:image" content="VideoPod.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://video-pod.vercel.app/" />
+
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Your Journey to Stunning Videos Starts Now" />
+        <meta name="twitter:description" content="Experience the ultimate toolkit for effortless video transformation" />
+        <meta name="twitter:image" content="VideoPod.png" />
       </head>
       <body>
         <SessionProvider>{children}</SessionProvider>
